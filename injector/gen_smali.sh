b339c520635ba7018697027167dc3301c04a1277
KuhakuPixel <NicholasPixels@gmail.com>
2023-01-08 14:19:49 +0700

    injector: copy only neccessary smali to smali_to_inject

--- injector/gen_smali.sh
+++ injector/gen_smali.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 PROJECT_ROOT_DIR=$(pwd)
+SMALI_TO_INJECT_DIR="smali_to_inject"
+GENERATED_SMALI_DIR="./smali/smali/com/example"
 # =================================================
+echo "Generating temp APK"
 # build apk via gradlew
 # https://stackoverflow.com/questions/24398041/build-android-studio-app-via-command-line
 # https://developer.android.com/studio/build/building-cmdline
@@ -17,7 +20,14 @@ cd "${PROJECT_ROOT_DIR}"
 
 
 # =================================================
+echo "decompiling temp APK"
 # decode without resources and 
 # put the smali results in smali folder
 # also force them using -f
 apktool d apk_source/hello-libs/app/build/outputs/apk/debug/app-debug.apk -r -f -o ./smali
+
+echo "Copying smali for injection"
+# put the smali for injection
+cp ${GENERATED_SMALI_DIR} ${SMALI_TO_INJECT_DIR} -r
+
+echo "Done!!!"
