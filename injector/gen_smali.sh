1c959dc2cc327f43b997dfc9ae7a835e9a2e8aca
KuhakuPixel <NicholasPixels@gmail.com>
2023-01-08 14:06:03 +0700

    injector: generate apk in gen_smali.sh

--- injector/gen_smali.sh
+++ injector/gen_smali.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# =================================================
+# build apk via gradlew
+# https://stackoverflow.com/questions/24398041/build-android-studio-app-via-command-line
+# https://developer.android.com/studio/build/building-cmdline
+./apk_source/gradlew assembleDebug
 
+# =================================================
 # decode without resources and 
 # put the smali results in smali folder
 # also force them using -f
