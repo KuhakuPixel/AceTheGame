f72dd648fe74f68de0db7fa1363552d5b9d51b6c
KuhakuPixel <NicholasPixels@gmail.com>
2023-01-20 19:35:26 +0700

    INJECTOR: fix injector  app example
    - add wifi permission to manifest because otherwise zmq cannot create a socket file
    (previously it will throw a permisison denied error in the `adb logcat`)
    
    - run a separate thread in injector's init function not in the native
    function, because creating thread in native function will still block
    the calling function from java (java function will wait for all cpp
    thread to exit I suppose)

--- injector/apk_source/hello-libs/app/src/main/java/com/example/utils/Injector.java
+++ injector/apk_source/hello-libs/app/src/main/java/com/example/utils/Injector.java
@@ -3,16 +3,29 @@ package com.example.utils;
 import android.util.Log;
 
 /*
-* static wrapper for code injection so no object instantiation
-* is needed
-* */
+ * static wrapper for code injection so no object instantiation
+ * is needed
+ * */
 public class Injector {
 
     public static void Init() {
-        Log.d("AceTheGame", "Code Is injected :D");
-        // inject the library
-        InjectorCore injectorCore = new InjectorCore();
-        injectorCore.Init();
+        Log.d("AceTheGame", "Code Injection Started :D");
+        Thread run_th = new Thread() {
+
+            public void run() {
+                // inject the library
+                InjectorCore injectorCore = new InjectorCore();
+                //TODO: make this multi thread, because we multithread
+                // in native module won't work and will make the app stuck
+                // in an inf loop, since the java ffi probably waits until
+                // all thread are stopped
+                injectorCore.Init();
+            }
+
+        };
+
+        run_th.start();
+        Log.d("AceTheGame", "Code Injection Done :D");
 
     }
 
