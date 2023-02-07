552db95b6b77e84c211793681af99bde30332c22
KuhakuPixel <NicholasPixels@gmail.com>
2023-02-07 12:47:23 +0700

    Modder: add test and fix DoesCommandExist

--- Modder/app/src/main/java/modder/Util.java
+++ Modder/app/src/main/java/modder/Util.java
@@ -9,30 +9,33 @@ public class Util {
 
     public static boolean DoesCommandExist(String command) {
         // TODO: add log warning when exception
-        //       - this way of checking is quite dangerous
-        //       - because it actually try to execute it 
+        // - this way of checking is quite dangerous
+        // - because it actually try to execute it
         Runtime runtime = Runtime.getRuntime();
         try {
             Process process = runtime.exec(command + " --help");
 
             try {
                 // wait for program to exit
-                // this is needed, because exec will run on 
-                // different thread 
+                // this is needed, because exec will run on
+                // different thread
                 // https://stackoverflow.com/questions/25979336/handling-the-illegalthreadstateexception
                 process.waitFor();
             } catch (InterruptedException interruptedException) {
+                // System.out.println("false, interrupted exception");
                 return false;
 
             }
             int exitCode = process.exitValue();
 
-            // 0 indicates normal termination, meaning the program exist
-            // in path
-            return exitCode == 0;
+            // if program is able to return a ret code
+            // then it exist
+            // System.out.println("exit code" + exitCode);
+            return true;
         }
 
         catch (IOException e) {
+            // System.out.println("false, IO exception");
             return false;
         }
 
