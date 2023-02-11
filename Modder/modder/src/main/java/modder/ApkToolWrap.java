e27a4d149edd8b42e4bb08f236b4f13d79aa17f2
KuhakuPixel <NicholasPixels@gmail.com>
2023-02-11 12:51:56 +0700

    Modder: add apktool

--- Modder/modder/src/main/java/modder/ApkToolWrap.java
+++ Modder/modder/src/main/java/modder/ApkToolWrap.java
@@ -1,8 +1,11 @@
 package modder;
+//import brut.*;
 
 import org.apache.commons.cli.*;
 //import brut.androlib.*;
-
+//import brut.apktool;
+//import brut.directory.*;
+import brut.common.BrutException;
 public class ApkToolWrap {
 
 	public static void ApkToolTest(String args[]) {
