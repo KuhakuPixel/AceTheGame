cd0e6a30728e79ad50117ea7d4f1fb6bc5600fe3
KuhakuPixel <NicholasPixels@gmail.com>
2023-02-12 11:53:22 +0700

    Modder: Use `ProcessBuilder` for `DoesCommandExist`
    it is much more simpler than `Runtime.Exec`
    and `Runtime.Exec` is about to be deprecated anyway
    
    https://bugs.openjdk.org/browse/JDK-8276412
    https://stackoverflow.com/questions/10723346/why-should-avoid-using-runtime-exec-in-java

--- Modder/modder/src/main/java/modder/Util.java
+++ Modder/modder/src/main/java/modder/Util.java
@@ -24,35 +24,18 @@ public class Util {
     }
 
     public static boolean DoesCommandExist(String command) {
-        // TODO: add log warning when exception
-        // - this way of checking is quite dangerous
-        // - because it actually try to execute it
-        Runtime runtime = Runtime.getRuntime();
+
+        // try to run command with arguments --helps,
+        // because most of program when given the argument will print help
+        // and exit immediately if it exist
+        ProcessBuilder procBuilder = new ProcessBuilder(command, "--help");
         try {
-            Process process = runtime.exec(command + " --help");
-
-            try {
-                // wait for program to exit
-                // this is needed, because exec will run on
-                // different thread
-                // https://stackoverflow.com/questions/25979336/handling-the-illegalthreadstateexception
-                process.waitFor();
-            } catch (InterruptedException interruptedException) {
-                // System.out.println("false, interrupted exception");
-                return false;
-
-            }
-            int exitCode = process.exitValue();
-
-            // if program is able to return a ret code
-            // then it exist
-            // System.out.println("exit code" + exitCode);
+            procBuilder.start();
             return true;
-        }
-
-        catch (IOException e) {
-            // System.out.println("false, IO exception");
+        } catch (IOException e) {
+            // thrown when command doesn't exist
             return false;
+
         }
 
     }
