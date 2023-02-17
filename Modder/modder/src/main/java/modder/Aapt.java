4d40503d13b9e4a467d00a20b3c677351ff4a802
KuhakuPixel <NicholasPixels@gmail.com>
2023-02-17 16:22:45 +0700

    Modder: add GetLaunchableActivity

--- Modder/modder/src/main/java/modder/Aapt.java
+++ Modder/modder/src/main/java/modder/Aapt.java
@@ -9,6 +9,7 @@ import brut.util.AaptManager;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.File;
+import org.apache.commons.lang3.StringUtils;
 
 public class Aapt {
 
@@ -25,5 +26,32 @@ public class Aapt {
 		output = Util.RunCommand(aaptFile.getAbsolutePath(), args);
 
 		return output;
+	}
+
+	public static String GetLaunchableActivity(String apkPath) {
+		String launchableActivity = "";
+
+		String args = String.format("dump badging %s", apkPath);
+		List<String> out = RunCmd(args);
+
+		for (int i = 0; i < out.size(); i++) {
+			// found a launchable activity
+			// ussually the output is like
+			//
+			// (in one line)
+			// ```
+			// launchable-activity: name='com.java.simpleapp.MainActivity' label='SimpleApp'
+			// icon=''
+			// ```
+			if (out.get(i).startsWith("launchable-activity:")) {
+				// try to parse the output and get the activity
+				String matchedStr[] = StringUtils.substringsBetween(out.get(i), "name='", "'");
+				// must get exactly one match
+				assert (matchedStr.length == 1);
+				launchableActivity = matchedStr[0];
+				break;
+			}
+		}
+		return launchableActivity;
 	}
 }
--- Modder/modder/src/test/java/modder/TestAapt.java
+++ Modder/modder/src/test/java/modder/TestAapt.java
@@ -15,5 +15,16 @@ class TestAapt {
 		output = Aapt.RunCmd("--help");
 		assertEquals(true, output.contains("Android Asset Packaging Tool"));
 		assertEquals(true, output.contains("Usage:"));
+
+	}
+
+	@Test
+	void GetLaunchableActivity() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		// https://stackoverflow.com/a/43415602/14073678
+		String filePath = classLoader.getResource("apk_example/app-debug.apk").getFile();
+
+		String launchableActivity = Aapt.GetLaunchableActivity(filePath);
+		assertEquals("com.java.simpleapp.MainActivity", launchableActivity);
 	}
 }
