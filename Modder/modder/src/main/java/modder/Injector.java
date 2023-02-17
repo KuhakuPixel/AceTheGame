923a30b4d6dabd1488592b63fda50c6c6ad0049b
KuhakuPixel <NicholasPixels@gmail.com>
2023-02-17 19:50:26 +0700

    MOdder: implement GetSmaliPathFromLaunchableActivity

--- Modder/modder/src/main/java/modder/Injector.java
+++ Modder/modder/src/main/java/modder/Injector.java
@@ -16,12 +16,34 @@ public class Injector {
 
 	public static String GetSmaliPathFromLaunchableActivity
 
-	(String launchableActivity, String decompiledApkFolder) {
+	(String launchableActivity, String decompiledApkDirStr) {
+
+		String relativeSmaliFilePath = GetSmaliSubPathFromLaunchableActivity(launchableActivity);
 
 		// when decompiling with apktool
 		// the smali classes in subPath will be contained in
 		// the folder starting with smali
 		// like smali, smali_classes2, smali_classes3 and ect
+		File decompiledApkDir = new File(decompiledApkDirStr);
+		File[] files = decompiledApkDir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+
+			if (!files[i].isDirectory())
+				continue;
+
+			String dirName = files[i].getName();
+
+			if (!dirName.startsWith("smali"))
+				continue;
+
+			String basePathStr = files[i].getAbsolutePath();
+			File smaliFile = new File(basePathStr, relativeSmaliFilePath);
+			// check if this thing actually exist
+			if (smaliFile.exists())
+				return smaliFile.getAbsolutePath();
+
+		}
 
 		return "";
 	}
