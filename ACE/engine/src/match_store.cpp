672b6b8a36e860b37157cb3c4b3274cfc8c323c0
KuhakuPixel <NicholasPixels@gmail.com>
2023-03-17 13:22:35 +0700

    ACE: remove code duplication in const and nonconst funcs
    Problem:
    	previously `match_storage::iterate` and
    	`match_storage::_iterate_val` has duplicate code
    	because they cannot use the same function
    
    	this will cause headache because well its duplicate cod3
    
    Solution:
    	in const method, `const_cast` the class
    	so calling non const in const is possiblehttps://stackoverflow.com/questions/8325400/how-to-call-a-non-const-method-from-a-const-method
    
    	https://stackoverflow.com/questions/5008541/how-to-call-a-non-const-function-within-a-const-function-c
    	https://stackoverflow.com/questions/8325400/how-to-call-a-non-const-method-from-a-const-method

--- ACE/engine/src/match_store.cpp
+++ ACE/engine/src/match_store.cpp
@@ -19,11 +19,9 @@ void match_storage<T>::set_scan_level(Scan_Utils::E_scan_level scan_level) {
 
 template <typename T>
 void match_storage<T>::_iterate(
-
     std::function<void(ADDR addr, T *val_ptr)> on_each_iteration_val_ptr,
-    std::function<void(ADDR addr, T val)> on_each_iteration_val, size_t max_iter
-
-) {
+    std::function<void(ADDR addr, T val)> on_each_iteration_val,
+    size_t max_iter) {
 
   // iterate each matches addresses
   size_t current_match_val_idx = 0;
@@ -39,13 +37,11 @@ void match_storage<T>::_iterate(
       // calculate current_addr
       ADDR current_addr = Scan_Utils::get_address_of_match_index<T>(
           this->scan_level, base_addr, j);
-      T *current_val_ptr = &(this->matches_value[current_match_val_idx]);
-      // call callback if only
-      // the callback is not NULL
-      if (on_each_iteration_val_ptr != NULL)
-        on_each_iteration_val_ptr(current_addr, current_val_ptr);
+      T *val_ptr = &(this->matches_value[current_match_val_idx]);
       if (on_each_iteration_val != NULL)
-        on_each_iteration_val(current_addr, *current_val_ptr);
+        on_each_iteration_val(current_addr, *val_ptr);
+      if (on_each_iteration_val_ptr != NULL)
+        on_each_iteration_val_ptr(current_addr, val_ptr);
     }
   }
 }
@@ -67,24 +63,18 @@ void match_storage<T>::iterate_val(
     std::function<void(ADDR addr, T val)> on_each_iteration,
     size_t max_iter) const {
 
-  // iterate each matches addresses
-  size_t current_match_val_idx = 0;
-  for (size_t i = 0; i < this->get_chunk_count(); i++) {
-    size_t match_chunk_matches_cnt = this->matches_records[i].match_count;
-    size_t base_addr = this->matches_records[i].base_addr;
-
-    for (size_t j = 0; j < match_chunk_matches_cnt;
-         j++, current_match_val_idx++) {
-      // iteration has reached max, exit from function
-      if (current_match_val_idx >= max_iter)
-        return;
-      // calculate current_addr
-      ADDR current_addr = Scan_Utils::get_address_of_match_index<T>(
-          this->scan_level, base_addr, j);
-      T val = this->matches_value[current_match_val_idx];
-      on_each_iteration(current_addr, val);
-    }
-  }
+  /*
+   * reuse non const function in const, this looks ugly but
+   * its better than code duplication
+   * https://stackoverflow.com/a/5008580/14073678
+   *
+   * some SO answer said that calling this function on a const object
+   * will yield undefined behavior
+   *
+   * https://stackoverflow.com/a/8325449/14073678
+   */
+  const_cast<match_storage<T> *>(this)->_iterate(NULL, on_each_iteration,
+                                                 max_iter);
 }
 
 template <typename T>
