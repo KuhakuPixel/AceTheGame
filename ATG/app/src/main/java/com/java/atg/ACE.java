67249a1aaf51eee51acf502fdffff70fbf2de5c4
KuhakuPixel <NicholasPixels@gmail.com>
2023-03-27 20:42:12 +0700

    ATG: add Cpu.java

---
+++ ATG/app/src/main/java/com/java/atg/ACE.java
@@ -0,0 +1,9 @@
+package com.java.atg;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class ACE {
+
+    public static final List<String> supportedCpu = Arrays.asList("x86", "x86_64", "armeabi-v7a", "arm64-v8a");
+}
