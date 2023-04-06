7eee2cf87c10feedf3cc617f262275ac52413352
KuhakuPixel <Nicholaspixels@gmail.com>
2023-04-06 14:21:08 +0700

    ACE: add option to send only 1 msg for engine client and print cleanup

--- ACE/engine/client/client.cpp
+++ ACE/engine/client/client.cpp
@@ -1,24 +1,47 @@
 #include "../src/ACE_global.hpp"
 #include "../src/engine_client.hpp"
 #include "../src/input.hpp"
+#include "../third_party/CLI11.hpp"
 #include <iostream>
 #include <stdio.h>
 #include <string>
 
-int main() {
+int main(int argc, char **argv) {
+
+  // ================ parse args =================
+  CLI::App main_app{
+      "ACE engine client: client for communicating to ACE's server"};
+
+  //
+  std::string msg_to_server = "";
+  CLI::Option *msg_option = main_app.add_option(
+      "--msg", msg_to_server,
+      "send message to engine's server and get reply via stdout\n");
+  //
+  CLI11_PARSE(main_app, argc, argv);
+  // ==============================================
   engine_client client =
       engine_client(ACE_global::engine_client_binded_address);
 
-  auto on_input = [&](std::string input_str) -> E_loop_statement {
-    // dont allow empty input
-    if (input_str.size() == 0)
-      return E_loop_statement::continue_;
-    //
-    std::string reply = client.request(input_str);
-    printf("\n%s\n", reply.c_str());
-    return E_loop_statement::continue_;
-  };
+  // only output one time  request
+  if (*msg_option) {
+    std::string reply = client.request(msg_to_server);
+    printf("%s", reply.c_str());
+  }
 
-  run_input_loop(on_input, "Engine Server");
+  else {
+    // run prompt
+    auto on_input = [&](std::string input_str) -> E_loop_statement {
+      // dont allow empty input
+      if (input_str.size() == 0)
+        return E_loop_statement::continue_;
+      //
+      std::string reply = client.request(input_str);
+      printf("\n%s\n", reply.c_str());
+      return E_loop_statement::continue_;
+    };
+
+    run_input_loop(on_input, "Engine Server");
+  }
   return 0;
 }
--- ACE/engine/src/engine_client.cpp
+++ ACE/engine/src/engine_client.cpp
@@ -9,13 +9,10 @@
 engine_client::engine_client(std::string client_binded_address) {
   this->client_binded_address = client_binded_address;
   // connect the socket to address
-  printf("Connecting to ACE engine server ...\n");
   this->socket.connect(client_binded_address);
-  printf("done\n");
 }
 
 std::string engine_client::request(std::string msg) {
-  printf("sending %s\n", msg.c_str());
   // make the request from string
   zmq::message_t request(msg.size());
   memcpy(request.data(), msg.c_str(), msg.size());
@@ -23,7 +20,6 @@ std::string engine_client::request(std::string msg) {
   //  Get the reply.
   zmq::message_t reply;
   auto receive_res = socket.recv(reply, zmq::recv_flags::none);
-  printf("done\n");
   if (!receive_res) {
     printf("Warning: Failed to receive message\n");
   }
