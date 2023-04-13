65f00f7fdb7130c44b58e3531c29052921e68c65
KuhakuPixel <Nicholaspixels@gmail.com>
2023-04-13 13:18:36 +0700

    ATG: create global ACE variable

--- ATG/app/src/main/java/com/java/atg/ATG.java
+++ ATG/app/src/main/java/com/java/atg/ATG.java
@@ -8,9 +8,20 @@ import java.io.IOException;
 
 public class ATG extends Application {
     private static Context context;
+    private static ACE ace = null;
+
     public void onCreate() {
         super.onCreate();
         ATG.context = getApplicationContext();
+        try {
+            ace = new ACE();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static ACE GetAce() {
+        return ace;
     }
 
     public static Context GetContext() {
--- ATG/app/src/main/java/com/java/atg/ProcessFragment.java
+++ ATG/app/src/main/java/com/java/atg/ProcessFragment.java
@@ -22,37 +22,19 @@ import java.util.List;
  */
 public class ProcessFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    //
     private final int rowItemCount = 3;
-
     public ProcessFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProcessFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProcessFragment newInstance(String param1, String param2) {
+    public static ProcessFragment newInstance() {
         ProcessFragment fragment = new ProcessFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     /**
@@ -77,14 +59,6 @@ public class ProcessFragment extends Fragment {
         return rowView;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -93,13 +67,7 @@ public class ProcessFragment extends Fragment {
         View mainView = inflater.inflate(R.layout.fragment_process, container, false);
         TableLayout procTable = mainView.findViewById(R.id.ProcessTable);
 
-        ACE ace = null;
-        try {
-            ace = new ACE();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        List<ProcInfo> runningProcs = ace.ListRunningProc();
+        List<ProcInfo> runningProcs = ATG.GetAce().ListRunningProc();
         for (int i = 0; i < runningProcs.size(); i++) {
             TableRow rowView = ProcInfoToTableRow(inflater, runningProcs.get(i), i);
             procTable.addView(rowView);
