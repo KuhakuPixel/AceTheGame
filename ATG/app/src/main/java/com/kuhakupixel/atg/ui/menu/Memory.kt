ce61661a3e19b2e60c0e0ca1c944c4b1f7ebdbb2
KuhakuPixel <Nicholaspixels@gmail.com>
2023-04-23 18:55:53 +0700

    ATG: Add scan input field

--- ATG/app/src/main/java/com/kuhakupixel/atg/ui/menu/Memory.kt
+++ ATG/app/src/main/java/com/kuhakupixel/atg/ui/menu/Memory.kt
@@ -1,18 +1,26 @@
 package com.kuhakupixel.atg.ui.menu
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kuhakupixel.atg.ui.GlobalConf
+import com.kuhakupixel.atg.ui.util.CheckboxWithText
 import com.kuhakupixel.atg.ui.util.CreateTable
+
+private var scanInputVal: MutableState<String> = mutableStateOf("")
+private var scanAgainstValue: MutableState<Boolean> = mutableStateOf(true)
 
 @Composable
 fun MemoryMenu(globalConf: GlobalConf?) {
@@ -26,14 +34,44 @@ fun MemoryMenu(globalConf: GlobalConf?) {
                 .weight(0.6f)
                 .padding(16.dp),
         )
-        MatchesSetting(modifier = Modifier.weight(0.4f))
+        MatchesSetting(
+            modifier = Modifier
+                .weight(0.4f)
+                .padding(10.dp)
+        )
     }
 }
 
 @Composable
 private fun MatchesSetting(modifier: Modifier = Modifier) {
+    @Composable
+    fun ScanInputField(scanValue: MutableState<String>, scanAgainstValue: MutableState<Boolean>) {
+        Row() {
+            CheckboxWithText(
+                modifier = Modifier.weight(0.45f),
+                checked = scanAgainstValue.value,
+                onCheckedChange = { value ->
+                    scanAgainstValue.value = value
+                },
+                text = "Against value",
+            )
+            TextField(
+                modifier = Modifier.weight(0.65f),
+                enabled = scanAgainstValue.value,
+                value = scanValue.value,
+                onValueChange = { value ->
+                    scanValue.value = value
+                },
+                label = { Text(text = "Scan For") },
+                placeholder = { Text(text = "value ...") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine=true,
+            )
+        }
+    }
+
     Column(modifier = modifier) {
-        Text("Memory")
+        ScanInputField(scanValue = scanInputVal, scanAgainstValue = scanAgainstValue)
     }
 }
 
