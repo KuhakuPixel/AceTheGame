335fa82fe887ee5f9cec528915bda20ab6ec48e4
KuhakuPixel <Nicholaspixels@gmail.com>
2023-04-24 17:32:59 +0700

    ACE: bugfix stopping server request
    Problem:
    	When stopping the request by sending "stop",
    	it will still call [on_input_received]
    	which will lead to an undesirable result :(
    
    Solution:
    
    	only call [on_input_received] if the request string is
    	not "stop"

--- ACE/engine/src/server.hpp
+++ ACE/engine/src/server.hpp
@@ -8,6 +8,8 @@ private:
   std::function<std::string(std::string input_str)> on_input_received;
 
 public:
+  // message to send to stop the server
+  static const std::string stop_request_str;
   /*
    *
    * [server_binded_address]: binded address of the server
@@ -16,7 +18,7 @@ public:
    * 			  should return a output
    *
    *
-   * server can be stopped by sending "stop" 
+   * server can be stopped by sending "stop"
    *
    * */
   server(std::string server_binded_address,
