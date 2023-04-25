bf3e3f2a141f49436d5a4896e55fb940a8343a5f
KuhakuPixel <Nicholaspixels@gmail.com>
2023-04-25 16:44:21 +0700

    ATG: add IsNumeric

--- ATG/app/src/main/java/com/kuhakupixel/atg/backend/NumUtil.java
+++ ATG/app/src/main/java/com/kuhakupixel/atg/backend/NumUtil.java
@@ -7,11 +7,20 @@ public class NumUtil {
      * Check whether [s] is a hexadecimal
      * it supports hexadecimal string that starts either or without "0x"
      * https://stackoverflow.com/questions/2670639/why-are-hexadecimal-numbers-prefixed-with-0x
-     * */
+     */
     public static boolean IsHex(String s) {
         s = s.toLowerCase();
         if (s.startsWith(HexStartFormat))
             s = s.substring(HexStartFormat.length());
         return s.matches("-?[0-9a-fA-F]+");
+    }
+
+    public static boolean IsNumeric(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- ATG/app/src/test/java/com/kuhakupixel/atg/NumUtilTest.java
+++ ATG/app/src/test/java/com/kuhakupixel/atg/NumUtilTest.java
@@ -29,8 +29,25 @@ public class NumUtilTest {
         Assert.assertFalse(NumUtil.IsHex("AnakinSkySleeper"));
         Assert.assertTrue(NumUtil.IsHex("0X7fffd4ba8f6c"));
 
-
-
+    }
+    @Test
+    public void IsNumeric() {
+        // =========== numeric ===============
+        Assert.assertTrue(NumUtil.IsNumeric("12"));
+        Assert.assertTrue(NumUtil.IsNumeric("15"));
+        Assert.assertTrue(NumUtil.IsNumeric("-15"));
+        Assert.assertTrue(NumUtil.IsNumeric("+15"));
+        Assert.assertTrue(NumUtil.IsNumeric("15.231"));
+        Assert.assertTrue(NumUtil.IsNumeric("-999.231"));
+        Assert.assertTrue(NumUtil.IsNumeric("-0.231"));
+        // ============= non numeric ==============
+        Assert.assertFalse(NumUtil.IsNumeric("a-0.231"));
+        Assert.assertFalse(NumUtil.IsNumeric("hello"));
+        Assert.assertFalse(NumUtil.IsNumeric("There"));
+        Assert.assertFalse(NumUtil.IsNumeric("0xaa"));
+        Assert.assertFalse(NumUtil.IsNumeric("++=2"));
+        Assert.assertFalse(NumUtil.IsNumeric("2..3"));
 
     }
+
 }
