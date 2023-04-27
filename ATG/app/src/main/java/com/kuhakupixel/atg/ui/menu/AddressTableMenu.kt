be6144e58933a9308c236d481b65b517ee6ae813
KuhakuPixel <Nicholaspixels@gmail.com>
2023-04-27 22:40:11 +0700

    ATG: add initial table in AddressTable

--- ATG/app/src/main/java/com/kuhakupixel/atg/ui/menu/AddressTableMenu.kt
+++ ATG/app/src/main/java/com/kuhakupixel/atg/ui/menu/AddressTableMenu.kt
@@ -1,27 +1,68 @@
 package com.kuhakupixel.atg.ui.menu
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kuhakupixel.atg.backend.ACE.MatchInfo
+import com.kuhakupixel.atg.backend.ACE.NumType
 import com.kuhakupixel.atg.ui.GlobalConf
+import com.kuhakupixel.atg.ui.util.CreateTable
+
+class AddressInfo(matchInfo: MatchInfo, numType: NumType) {
+
+}
 
 @Composable
 fun AddressTableMenu(globalConf: GlobalConf?) {
-    Box(
+
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxSize(),
-        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Address Table",
-            fontWeight = FontWeight.Bold,
+        Text("Address Table", modifier = Modifier.weight(0.2f))
+        SavedAddressesTable(modifier = Modifier
+            .weight(0.8f)
+            .padding(16.dp)
         )
+
     }
+}
+
+@Composable
+fun SavedAddressesTable(
+    modifier: Modifier = Modifier,
+) {
+    CreateTable(
+        modifier = modifier,
+        colNames = listOf("Address", "Value"),
+        colWeights = listOf(0.4f, 0.6f),
+        itemCount = 0,
+        minEmptyItemCount = 50,
+        onRowClicked = { rowIndex: Int ->
+
+        },
+        drawCell = { rowIndex: Int, colIndex: Int, cellModifier: Modifier ->
+            /*
+            if (colIndex == 0) {
+                Text(text = matches.value[rowIndex].address, modifier = cellModifier)
+            }
+            if (colIndex == 1) {
+                Text(text = matches.value[rowIndex].prevValue, modifier = cellModifier)
+            }
+             */
+        }
+    )
+
 }
 
 @Composable
