cd1ae86661e30a9adf956cb3f1efb1737f3b4c7c
KuhakuPixel <Nicholaspixels@gmail.com>
2023-05-03 18:07:27 +0700

    ATG: add input dialog

--- ATG/app/src/main/java/com/kuhakupixel/atg/ui/util/Dialog.kt
+++ ATG/app/src/main/java/com/kuhakupixel/atg/ui/util/Dialog.kt
@@ -2,26 +2,33 @@ package com.kuhakupixel.atg.ui.util
 
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.text.input.KeyboardType
 
 /**
  * [onClose]: Called when dialog is dismissed or confirmed
  * [onConfirm]: Called when confirmed
  * */
 @Composable
-fun _ShowDialog(
+fun ShowDialog(
     title: String,
-    msg: String,
+    body: @Composable () -> Unit,
     onClose: () -> Unit,
     onConfirm: () -> Unit,
-    showCancelButton: Boolean = false
+    showCancelButton: Boolean
+
 ) {
+
     AlertDialog(
         onDismissRequest = {
             onClose()
@@ -29,14 +36,7 @@ fun _ShowDialog(
         title = {
             Text(title)
         },
-        text = {
-            Text(
-                msg,
-                modifier = Modifier
-                    .verticalScroll(rememberScrollState())
-                    .horizontalScroll(rememberScrollState()),
-            )
-        },
+        text = body,
         confirmButton = {
             Button(
                 onClick = {
@@ -56,22 +56,72 @@ fun _ShowDialog(
             }
         },
     )
+}
 
+@Composable
+fun InputValueDialog(
+    title: String,
+    onClose: () -> Unit,
+    onConfirm: (input: String) -> Unit,
+    showCancelButton: Boolean = false
+) {
+    var valueInput: MutableState<String> = remember { mutableStateOf("") }
+    ShowDialog(
+        title = title,
+        body = {
+            TextField(
+                value = valueInput.value,
+                onValueChange = { value ->
+                    valueInput.value = value
+                },
+                label = { Text(text = "Value Input ...") },
+                placeholder = { Text(text = "value ...") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+            )
+        },
+        onClose = onClose,
+        onConfirm = { onConfirm(valueInput.value) },
+        showCancelButton = showCancelButton,
+    )
+}
+
+@Composable
+fun ShowTextDialog(
+    title: String,
+    msg: String,
+    onClose: () -> Unit,
+    onConfirm: () -> Unit,
+    showCancelButton: Boolean = false
+) {
+
+    ShowDialog(
+        title = title,
+        body = {
+            Text(
+                msg,
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .horizontalScroll(rememberScrollState()),
+            )
+        },
+        onClose = onClose, onConfirm = onConfirm, showCancelButton = showCancelButton,
+    )
 }
 
 @Composable
 fun InfoDialog(msg: String, onClose: () -> Unit, onConfirm: () -> Unit) {
-    _ShowDialog(title = "Info", msg = msg, onClose = onClose, onConfirm = onConfirm)
+    ShowTextDialog(title = "Info", msg = msg, onClose = onClose, onConfirm = onConfirm)
 }
 
 @Composable
 fun WarningDialog(msg: String, onClose: () -> Unit, onConfirm: () -> Unit) {
-    _ShowDialog(title = "Warning", msg = msg, onClose = onClose, onConfirm = onConfirm)
+    ShowTextDialog(title = "Warning", msg = msg, onClose = onClose, onConfirm = onConfirm)
 }
 
 @Composable
 fun ErrorDialog(msg: String, onClose: () -> Unit, onConfirm: () -> Unit) {
-    _ShowDialog(title = "Error", msg = msg, onClose = onClose, onConfirm = onConfirm)
+    ShowTextDialog(title = "Error", msg = msg, onClose = onClose, onConfirm = onConfirm)
 }
 
 @Composable
@@ -81,7 +131,7 @@ fun ConfirmDialog(
     onClose: () -> Unit,
     onConfirm: () -> Unit,
 ) {
-    _ShowDialog(
+    ShowTextDialog(
         title = title,
         msg = msg,
         onClose = onClose,
