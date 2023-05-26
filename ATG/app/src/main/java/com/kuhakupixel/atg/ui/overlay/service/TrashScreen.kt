cab52e27b4725f4c21ad7b49023ce66c4198b707
KuhakuPixel <Nicholaspixels@gmail.com>
2023-05-26 17:23:16 +0700

    ATG: move trash screen to one file

--- ATG/app/src/main/java/com/kuhakupixel/atg/ui/overlay/service/TrashScreen.kt
+++ ATG/app/src/main/java/com/kuhakupixel/atg/ui/overlay/service/TrashScreen.kt
@@ -1,13 +1,29 @@
 package com.kuhakupixel.atg.ui.overlay.service
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
 import com.kuhakupixel.atg.ui.overlay.composables.Trash
 
+@Composable
+fun TrashContentScreen(showOverlayButton: Boolean) {
+    val state = LocalServiceState.current
+
+    Box(Modifier.onGloballyPositioned {
+        state.screenWidthPx = it.size.width
+        state.screenHeightPx = it.size.height
+    }) {
+        // future.txt correct z-order
+        if (showOverlayButton) {
+            ShowTrash()
+        }
+    }
+}
 @Composable
 fun ShowTrash() {
     val serviceState = LocalServiceState.current
