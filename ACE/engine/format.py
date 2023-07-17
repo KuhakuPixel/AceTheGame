678e4c9d39a95ec2d0ab4e4e5cfd5129defd3a18
kuhakupixel <nicholaspixels@gmail.com>
2023-07-17 11:47:19 +0700

    ACE: refactor format.py

--- ACE/engine/format.py
+++ ACE/engine/format.py
@@ -6,29 +6,40 @@ import subprocess
 import glob, os
 
 
-def find_file_with_extension(dir_name:str, extensions=[]):
-    files = []
-    for file in os.listdir(dir_name):
-        file_abs_path = os.path.abspath(os.path.join(dir_name, file))
-        for ext in extensions:
-            if file_abs_path.endswith(ext):
-                # print("formatting: " + file_abs_path)
-                files.append(file_abs_path)
+def find_file_with_extension(dir_name: str, extensions=[]):
+    files_with_extension = []
+    for path, subdir, files in os.walk(dir_name):
+        #
+        for file_name in files:
+            file_abs_path = os.path.join(path, file_name)
+            # check if file has the extension and include them
+            for ext in extensions:
+                if file_abs_path.endswith(ext):
+                    files_with_extension.append(file_abs_path)
 
-    return files
-
-
-def format_c_cpp_dir(dir_name: str):
-    extensions = [".cpp", ".hpp"]
-    files_to_format = find_file_with_extension(dir_name=dir_name,extensions=extensions)
-
-    for file in files_to_format:
-        # -i for inplace format
-        cmd = f"clang-format -i --verbose {file}"
-        subprocess.run(cmd.split(" "))
+    return files_with_extension
 
 
+def format_c_cpp_dir(root_dir: str, extensions: list, dir_to_exclude: list):
+    dirs = []
+    # only pick directory name not in [dir_to_exclude]
+    for f in os.listdir(root_dir):
+        full_dir_path = os.path.join(root_dir, f)
+        if os.path.isdir(full_dir_path):
+            if f not in dir_to_exclude:
+                dirs.append(full_dir_path)
 
-dirs_to_format = ["./src/", "./client/", "./test/"]
-for _dir in dirs_to_format:
-    format_c_cpp_dir(_dir)
+    # format recursively for each directory in [dirs]
+    for d in dirs:
+        files_to_format = find_file_with_extension(dir_name=d, extensions=extensions)
+        for file in files_to_format:
+            # -i for inplace format
+            cmd = f"clang-format -i --verbose {file}"
+            subprocess.run(cmd.split(" "))
+
+
+EXTENSIONS = [".cpp", ".hpp"]
+DIRS_TO_EXCLUDE = ["third_party"]
+format_c_cpp_dir(
+    root_dir=os.getcwd(), extensions=EXTENSIONS, dir_to_exclude=DIRS_TO_EXCLUDE
+)
