1190173637c4efff677ed8db65b81ca2ba968549
kuhakupixel <nicholaspixels@gmail.com>
2023-09-09 13:39:54 +0700

    Modder: add option to not clean decompiled folder during patch to make debugging easier

--- Modder/modder/src/main/java/modder/Patcher.kt
+++ Modder/modder/src/main/java/modder/Patcher.kt
@@ -13,7 +13,7 @@ import java.nio.file.Paths
 // called MemScanner 
 class Patcher(
         apkFilePathStr: String,
-        tempFolderTaskOnExit: TempManager.TaskOnExit = TempManager.TaskOnExit.clean,
+        cleanDecompilationOnExit: Boolean = true,
         val decodeResource: Boolean,
 ) {
     var apkFilePathStr: String
@@ -26,12 +26,12 @@ class Patcher(
         Assert.AssertExistAndIsFile(apkFile)
         // make sure to get the absolute path
         this.apkFilePathStr = apkFile.absolutePath
-        val tempDir = TempManager.CreateTempDirectory("ModderDecompiledApk", tempFolderTaskOnExit)
+        val tempDir = TempManager.CreateTempDirectory("ModderDecompiledApk", cleanDecompilationOnExit)
         // make sure we have the absolute path
         // https://stackoverflow.com/a/17552395/14073678
         decompiledApkDirStr = tempDir.toAbsolutePath().toString()
         // =============================== decompile the apk ===========
-        logger.info{"decompiled at ${decompiledApkDirStr}"}
+        logger.info { "decompiled at ${decompiledApkDirStr}" }
         ApkToolWrap.Decompile(apkFilePathStr, decompiledApkDirStr, decodeResource = this.decodeResource)
     }
 
@@ -232,22 +232,25 @@ class Patcher(
 
 
     fun AddMemScannerSmaliCode() {
-        // path to copy the smali constructor to
-        val smaliCodePackageDir = GetPackageDirOfLaunchableActivity()
 
-        // copy the zip code of smali constructor from resources
-        // unextract it in a temp folder and then copy to
-        // the apk
+        /**
+         * copy the zip code of smali constructor from resources
+         * to a temp folder and then extract to the apk
+         */
+
+        // copy zip to temp folder
         val tempDir: String = TempManager.CreateTempDirectory("TempSmalifolder").toString()
-        //
         val destSmaliZipCode = File(tempDir, MEM_SCANNER_SMALI_ZIP_NAME)
         resource.CopyResourceFile(MEM_SCANNER_SMALI_CODE_ZIP_PATH, destSmaliZipCode.absolutePath)
+        // path to copy the smali code to
+        val smaliCodePackageDir = GetPackageDirOfLaunchableActivity()
+        //
         val destDir = File(smaliCodePackageDir, MEM_SCANNER_SMALI_DIR_NAME).absolutePath
-        val zipFile = ZipFile(destSmaliZipCode.absolutePath)
-        zipFile.extractAll(destDir)
-        System.out.printf("extracted to %s\n", destDir)
-        zipFile.close()
-        System.out.printf("copying resource to %s\n", destDir)
+        ZipFile(destSmaliZipCode.absolutePath).use { zipFile: ZipFile ->
+            zipFile.extractAll(destDir)
+            zipFile.close()
+        }
+        logger.info { "extracted to ${destDir}" }
     }
 
 
@@ -259,12 +262,11 @@ class Patcher(
         // server to the init function of smali launchable file
         val entrySmaliPathStr = GetEntrySmaliPath()
         val entrySmaliPath = Paths.get(entrySmaliPathStr)
-        println("using log4j")
         logger.info { "entry smali file: ${entrySmaliPathStr}" }
         val modifiedSmaliCode = AddMemScannerConstructorSmaliCode(entrySmaliPathStr)
-        logger.info { "========== modified smali code ========================" }
-        logger.info { modifiedSmaliCode.joinToString(separator = "\n") }
-        logger.info { "==================================================" }
+        //logger.info { "========== modified smali code ========================" }
+        //logger.info { modifiedSmaliCode.joinToString(separator = "\n") }
+        //logger.info { "==================================================" }
         // rewrite file
         Files.write(entrySmaliPath, modifiedSmaliCode)
     }
@@ -321,10 +323,11 @@ class Patcher(
 
         // smali code
         const val MEM_SCANNER_SMALI_DIR_NAME = "AceInjector"
+        // code to inject's path from resource
         val MEM_SCANNER_SMALI_BASE_DIR = "/" + java.lang.String.join("/", "AceAndroidLib", "code_to_inject", "smali", "com")
         const val MEM_SCANNER_SMALI_ZIP_NAME = MEM_SCANNER_SMALI_DIR_NAME + ".zip"
-        val MEM_SCANNER_SMALI_RESOURCE_DIR = File(MEM_SCANNER_SMALI_BASE_DIR, MEM_SCANNER_SMALI_ZIP_NAME).absolutePath
         val MEM_SCANNER_SMALI_CODE_ZIP_PATH = java.lang.String.join("/", MEM_SCANNER_SMALI_BASE_DIR, MEM_SCANNER_SMALI_ZIP_NAME)
+        // smali code to start the service
         const val MEM_SCANNER_CONSTRUCTOR_SMALI_CODE = "invoke-static {}, Lcom/AceInjector/utils/Injector;->Init()V"
 
         fun LaunchableActivityToSmaliRelativePath(launchableActivity: String): String {
